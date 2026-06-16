49ed3a4fb7e29c5eb1b9a6d6741c55128b605da0
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2026-06-16 23:06:45 +0300

    migration

---
+++ Migrations/20260616200344_InitialCreate.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace iroh_be.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "company",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    firsthourprice = table.Column<int>(type: "integer", nullable: false),
+                    additionalhalfhourprice = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_company", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customers",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    lastname = table.Column<string>(type: "text", nullable: true),
+                    phone = table.Column<string>(type: "text", nullable: true),
+                    mail = table.Column<string>(type: "text", nullable: true),
+                    createdat = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updatedat = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    isdeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "packages",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    hours = table.Column<decimal>(type: "numeric", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    validity_days = table.Column<int>(type: "integer", nullable: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_packages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tables",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    isdeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tables", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    lastname = table.Column<string>(type: "text", nullable: true),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: true),
+                    mail = table.Column<string>(type: "text", nullable: false),
+                    isactive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "children",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    parent_id = table.Column<int>(type: "integer", nullable: false),
+                    birth_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_children", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_children_customers_parent_id",
+                        column: x => x.parent_id,
+                        principalSchema: "public",
+                        principalTable: "customers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "purchases",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hours = table.Column<decimal>(type: "numeric", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    createdat = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updatedat = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    customerid = table.Column<int>(type: "integer", nullable: false),
+                    startdate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    enddate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_purchases", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_purchases_customers_customerid",
+                        column: x => x.customerid,
+                        principalSchema: "public",
+                        principalTable: "customers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bookings",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    tableid = table.Column<int>(type: "integer", nullable: true),
+                    starttime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    endtime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: true),
+                    note = table.Column<string>(type: "text", nullable: true),
+                    subscriptionstarttime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    subscriptionendtime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    child_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bookings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_bookings_children_child_id",
+                        column: x => x.child_id,
+                        principalSchema: "public",
+                        principalTable: "children",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_bookings_tables_tableid",
+                        column: x => x.tableid,
+                        principalSchema: "public",
+                        principalTable: "tables",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "purchasepayments",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hours = table.Column<decimal>(type: "numeric", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    purchaseid = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_purchasepayments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_purchasepayments_purchases_purchaseid",
+                        column: x => x.purchaseid,
+                        principalSchema: "public",
+                        principalTable: "purchases",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bookinglogs",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    bookingid = table.Column<int>(type: "integer", nullable: false),
+                    time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    userid = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bookinglogs", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_bookinglogs_bookings_bookingid",
+                        column: x => x.bookingid,
+                        principalSchema: "public",
+                        principalTable: "bookings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "purchasebookings",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    bookingid = table.Column<int>(type: "integer", nullable: false),
+                    purchaseid = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_purchasebookings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_purchasebookings_bookings_bookingid",
+                        column: x => x.bookingid,
+                        principalSchema: "public",
+                        principalTable: "bookings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_purchasebookings_purchases_purchaseid",
+                        column: x => x.purchaseid,
+                        principalSchema: "public",
+                        principalTable: "purchases",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookinglogs_bookingid",
+                schema: "public",
+                table: "bookinglogs",
+                column: "bookingid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_child_id",
+                schema: "public",
+                table: "bookings",
+                column: "child_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_tableid",
+                schema: "public",
+                table: "bookings",
+                column: "tableid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_children_parent_id",
+                schema: "public",
+                table: "children",
+                column: "parent_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_purchasebookings_bookingid",
+                schema: "public",
+                table: "purchasebookings",
+                column: "bookingid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_purchasebookings_purchaseid",
+                schema: "public",
+                table: "purchasebookings",
+                column: "purchaseid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_purchasepayments_purchaseid",
+                schema: "public",
+                table: "purchasepayments",
+                column: "purchaseid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_purchases_customerid",
+                schema: "public",
+                table: "purchases",
+                column: "customerid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bookinglogs",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "company",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "packages",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "purchasebookings",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "purchasepayments",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "bookings",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "purchases",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "children",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "tables",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "customers",
+                schema: "public");
+        }
+    }
+}
